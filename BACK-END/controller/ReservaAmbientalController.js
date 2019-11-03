ee3b3aa74c67bdf88d9c4026c15776d2a4207c70
Gabriel Soares <gabriel.soares.a@hotmail.com>
2019-11-03 18:37:15 -0300

    Progresso Controller

--- BACK-END/controller/ReservaAmbientalController.js
+++ BACK-END/controller/ReservaAmbientalController.js
@@ -7,12 +7,9 @@ var mysql = require('mysql');
 class ReservaAmbientalController{
     
     constructor() {
-        let $ = document.querySelector.bind(document);
-        this._data_input = $('#data');
-        this._qtde_input = $('#quantidade');
-        this._valor_input = $('#valor');
-
-        this._lista_negociacoes = new ListaNegociacoes();
+        this.$ = document.querySelector.bind(document);
+        this._conn = this._connect();
+        this._reserva = null;
     }
 
     _connect() {
@@ -31,17 +28,50 @@ class ReservaAmbientalController{
         return(conn);
     }
 
-    cadastra(){
-        conn = this._connect();
+    insert(){
+        this._reserva = new ReservaAmbiental($('#usuario').value, $('#nome').value, $('#local').value, $('#tamanho').value, $('#tipo').value, $('#saude').value);
 
-        var query = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')"
+        if(this._conn.state == 'disconnected'){
+            console.log("Error ReservaAmbientalController.insert(): MySQL connection object state is disconnected.");
+        }
+        else{
+            var query = "INSERT INTO reservas_ambientais (reservas_id_usuario, nome, local, tamanho, tipo_reserva, saude_reserva) VALUES (" + this._reserva.usuario() + ", " + this._reserva.nome() + ", " + this._reserva.local() + ", " + this._reserva.tamanho() + ", " + this._reserva.tipo() + ", " + this._reserva.saude() + ")"
+        
+            this._con.query(query, (err, result) => {
+                if (err) throw err;
+                console.log("Successfully inserted into reservas_ambientais.");
+            });
+        }
     }
 
-    _limpaForm(){
-        this._data_input.value = '';
-        this._qtde_input.value = 1;
-        this._valor_input.value = 0.0;
+    select(){
+        let id_select = $('#id').value;
+        let usuario_select = $('#usuario').value;
+        let nome_select = $('#nome').value;
+        let local_select = $('#local').value;
+        let tamanho_select = $('#tamanho').value;
+        let tipo_select = $('#tipo').value;
+        let saude_select = $('#saude').value);
 
-        this._data_input.focus();
+        if(this._conn.state == 'disconnected'){
+            console.log("Error ReservaAmbientalController.select(): MySQL connection object state is disconnected.");
+        }
+        else{
+            // QUERY NÃO FINALIZADA
+            var query = "SELECT * FROM reservas_ambientais WHERE )";
+        
+            this._con.query(query, (err, result, fields) => {
+                if (err) throw err;
+                console.log("Successfully selected from reservas_ambientais.");
+            });
+        }
+    }
+
+    _preencheTela(){
+
+    }
+
+    update(){
+
     }
 }
