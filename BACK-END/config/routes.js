0135db962f9a193b9b568c2c9b8a9e6bee253e16
weile123 <mah.dejesus@gmail.com>
2019-11-09 00:37:49 -0300

    Change in index route and index creation

--- BACK-END/config/routes.js
+++ BACK-END/config/routes.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     path =  require('path');
 
-  app.route('/home')
+  app.route('/')
     .get((req, res) => {
       res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
     });
