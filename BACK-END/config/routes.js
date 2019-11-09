91600648d0003f97d93a516617a8585b223ac657
weile123 <mah.dejesus@gmail.com>
2019-11-09 02:27:02 -0300

    voltando route ao original

--- BACK-END/config/routes.js
+++ BACK-END/config/routes.js
@@ -2,10 +2,8 @@ var express = require('express'),
     app = express(),
     path =  require('path');
 
-  // app.route('/')
-  //   .get((req, res) => {
-  //     res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
-  //   });
-
-  app.use('/', express.static(path.join(__dirname, '../../FRONT-END/html')));
+  app.route('/')
+    .get((req, res) => {
+      res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
+    });
 module.exports = app;
