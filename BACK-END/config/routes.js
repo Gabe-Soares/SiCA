4f0934738e9f5de6d680d6cbb923503baf5b791a
weile123 <mah.dejesus@gmail.com>
2019-11-09 02:23:53 -0300

    Inicio tela inicial

--- BACK-END/config/routes.js
+++ BACK-END/config/routes.js
@@ -2,9 +2,10 @@ var express = require('express'),
     app = express(),
     path =  require('path');
 
-  app.route('/')
-    .get((req, res) => {
-      res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
-    });
+  // app.route('/')
+  //   .get((req, res) => {
+  //     res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
+  //   });
 
+  app.use('/', express.static(path.join(__dirname, '../../FRONT-END/html')));
 module.exports = app;
