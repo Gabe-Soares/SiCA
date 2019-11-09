3eb2e9b6372cc15b9f273b652aab0755c9310c6a
Gabriel Soares <gabriel.soares.a@hotmail.com>
2019-11-09 05:56:22 -0300

    Progresso APIs ReservaAmbiental

--- BACK-END/config/routes.js
+++ BACK-END/config/routes.js
@@ -1,24 +1,31 @@
+// Declaração das variáveis.
 var express = require('express'),
     app = express(),
     path =  require('path'),
     bodyParser = require('body-parser'),
     ReservaAmbientalController = require('../controller/ReservaAmbientalController');
 
-  app.use(bodyParser.json());
-  app.use('/js', express.static(path.join(__dirname, '../../FRONT-END/js')));
-  app.use('/html', express.static(path.join(__dirname, '../../FRONT-END/html')));
-  app.use('/css', express.static(path.join(__dirname, '../../FRONT-END/css')));
+// Declaração dos arquivos externos que o Express.js irá chamar.
+app.use(bodyParser.json());
+app.use('/js', express.static(path.join(__dirname, '../../FRONT-END/js')));
+app.use('/html', express.static(path.join(__dirname, '../../FRONT-END/html')));
+app.use('/css', express.static(path.join(__dirname, '../../FRONT-END/css')));
 
-  app.route('/index.html')
-  .get((req, res) => {
-    res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
-  });
+// Rota para chamada inicial do Front-End / home.
+app.route('/index.html')
+.get((req, res) => {
+  res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
+});
 
-  app.route('/')
-  .get((req, res) => {
-    res.redirect('/index.html');
-  });
+// Rota de redirecionamento para a home.
+app.route('/')
+.get((req, res) => {
+  res.redirect('/index.html');
+});
 
-  app.route('/api/ReservaAmbiental/Cadastro/').post(ReservaAmbientalController.insert);
+// Definição de rotas para as APIs.
+app.route('/api/ReservaAmbiental/Cadastro/').post(ReservaAmbientalController.insert);
+app.route('/api/ReservaAmbiental/ConsultaId/').post(ReservaAmbientalController.selectId);
+app.route('/api/ReservaAmbiental/ConsultaNome/').post(ReservaAmbientalController.selectNome);
 
 module.exports = app;
--- BACK-END/controller/ReservaAmbientalController.js
+++ BACK-END/controller/ReservaAmbientalController.js
@@ -1,9 +1,5 @@
-//  TODO:
-//      - Tratamento de exceções
-
 // Imports
 var mysql = require('mysql');
-require('../model/ReservaAmbiental');
 
 function connect(res){
     var conn = mysql.createConnection({
@@ -30,8 +26,7 @@ function connect(res){
 exports.insert = function (req, res){
     conn = connect(res);
 
-    // var query = "INSERT INTO reservas_ambientais (reservas_id_usuario, nome, local, tamanho, tipo_reserva, saude_reserva) VALUES (" + req.body.usuario + ", " + req.body.nome + ", " + req.body.local + ", " + req.body.tamanho + ", " + req.body.tipo + ", " + req.body.saude + ")"
-    var query = "INSERT INTO teste values ( " + req.body.id + ")"
+    var query = "INSERT INTO reservas_ambientais (reservas_id_usuario, nome, local, tamanho, tipo_reserva, saude_reserva) VALUES (" + req.body.usuario + ", " + req.body.nome + ", " + req.body.local + ", " + req.body.tamanho + ", " + req.body.tipo + ", " + req.body.saude + ")";
 
     conn.query(query, (err, result) => {
         if (err) {
@@ -48,27 +43,44 @@ exports.insert = function (req, res){
     });
 }
 
-exports.select = function (req, res){
-    conn = connect();
+exports.selectId = function (req, res){
+    conn = connect(res);
 
-    let id_select = $('#id').value;
-    let usuario_select = $('#usuario').value;
-    let nome_select = $('#nome').value;
-    let local_select = $('#local').value;
-    let tamanho_select = $('#tamanho').value;
-    let tipo_select = $('#tipo').value;
-    let saude_select = $('#saude').value;
+    if(req.body.id)
+        var query = "SELECT * FROM reservas_ambientais WHERE id_reservas = " + req.body.id;
 
-    if(conn.state == 'disconnected'){
-        console.log("Error ReservaAmbientalController.select(): MySQL connection object state is disconnected.");
-    }
-    else{
-        // QUERY NÃO FINALIZADA
-        var query = "SELECT * FROM reservas_ambientais WHERE )";
-    
-        conn.query(query, (err, result, fields) => {
-            if (err) throw err;
-            console.log("Successfully selected from reservas_ambientais.");
-        });
-    }
+    conn.query(query, (err, result, fields) => {
+        if (err) {
+            console.log("Failed to select from reservas_ambientais.");
+            res.status(503);
+            res.end();
+            throw err;
+        }
+        else{
+            console.log("Successfully selected data from reservas_ambientais.");
+            res.status(200);
+            res.json(result);
+        }
+    });
 }
+
+exports.selectNome = function (req, res){
+    conn = connect(res);
+
+    var query = "SELECT * FROM reservas_ambientais WHERE nome LIKE %" + req.body.nome + "%";
+
+    conn.query(query, (err, result, fields) => {
+        if (err) {
+            console.log("Failed to select from reservas_ambientais.");
+            res.status(503);
+            res.end();
+            throw err;
+        }
+        else{
+            console.log("Successfully selected data from reservas_ambientais.");
+            res.status(200);
+            res.json(result);
+        }
+    });
+}
+
