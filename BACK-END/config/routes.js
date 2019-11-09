85839f97125d6c88888b7c2e7a66ffabbd3e9829
Gabriel Soares <gabriel.soares.a@hotmail.com>
2019-11-09 02:35:49 -0300

    Committing route.js changes

--- BACK-END/config/routes.js
+++ BACK-END/config/routes.js
@@ -7,10 +7,7 @@ var express = require('express'),
     .get((req, res) => {
       res.sendFile(path.join(__dirname + '../../../FRONT-END/html/index.html'));
     });
-<<<<<<< HEAD
 
 app.route('/api/ReservaAmbiental/Cadastro/').post(ReservaAmbientalController.insert);  
   
-=======
->>>>>>> 91600648d0003f97d93a516617a8585b223ac657
 module.exports = app;
